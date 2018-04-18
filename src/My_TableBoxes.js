06b48e99da688ef16811fcdd2c9a5076be0b3435
Kimberley Hayward <haywarkimb@ecs.vuw.ac.nz>
2018-04-18 22:48:37 +1200

    Added code for potential new way to display vehicles.

---
+++ src/My_TableBoxes.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class My_TableBoxes extends Component {
+    render() {
+        return(
+            <div class="flex-box">
+            <div>
+                <div class="menu-item">Box One</div>
+                <div class="menu-item">Box Two</div>
+            </div>
+            {/* <div>
+                <div class="menu-item">Bookings</div>
+                <div class="menu-item">Summary</div>
+            </div> */}
+            </div>
+        )
+    }
+}
+
+export default My_TableBoxes;
