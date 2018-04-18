f0a7b911cfe294ff3a44f4ba162103958b94b7a5
Kimberley Hayward <haywarkimb@ecs.vuw.ac.nz>
2018-04-18 22:48:08 +1200

    Added a nav bar, which is not yet functional.

---
+++ src/My_NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class My_NavBar extends Component{
+    render(){
+        return (
+            <div class="HorzNav">
+                <a href="#Vehicles" class="active">Vehicles</a>
+                <a href="#Add">Add Vehcile</a>
+                <a href="#Upcoming">Upcoming Events</a>
+            </div>
+        )
+    } 
+}
+export default My_NavBar; 
