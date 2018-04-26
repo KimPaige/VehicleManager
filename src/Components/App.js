3c523d30d3a64b63a665a0707c857a8b9d64f155
Kimberley Hayward <haywarkimb@ecs.vuw.ac.nz>
2018-04-26 18:56:59 +1200

    Added responsive navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import My_Header from './My_Header';
 import My_Footer from './My_Footer';
 import Main from './Main';
+import My_NavBar from './My_NavBar';
 
 class App extends Component {
     render (){
         return (
             <div>
             <My_Header/>
+            <My_NavBar/>
             <Main/>
             <My_Footer/>
             </div>
